dd837a63f3c5f06284332cf3ede051804ec97e84
Eros <eros.gear@gmail.com>
2025-07-02 23:04:48 -0700

    api intengrations finished, only the accesability remains

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,15 +7,25 @@ import Footer from './footer/footer.jsx'
 import api from '../utils/api.js'
 import { CurrentUserContext } from '../contexts/CurrentUserContext.js'
 function App() {
+  //estados
   const [currentUser , setUser] = useState({});
   const [popup, setPopup] = useState(null);
+  const [cards, setCards] = useState([]);
+  //api para usuario
   useEffect(() => {
     api.getUserInfo().then((res) => {
       console.log(`user = ${res}`);
       setUser(res);
     })
   }, []);
-
+//api para las targetas 
+  useEffect(() => {
+    api.getInitialCards().then((res) => {
+      console.log(res);
+      setCards(res);
+    })
+  }, []);
+//user handlers
   const handleUserUpdate = async (user) => {
    const updatedUser = await api.editProfile(user);
    setUser(updatedUser);
@@ -30,21 +40,52 @@ function App() {
     handleClosePopup();
 
   }
-
+//popup handlers
   function handleOpenPopup(popup) {
     setPopup(popup);
   }
   function handleClosePopup() {
     setPopup(null);
   }
+  //card handlers
+  async function handleCardLike(card) {
+    const isLiked = card.isLiked;
+  
+    const newCard = isLiked
+      ? await api.removeLike(card._id)
+      : await api.addLike(card._id);
+  
+    setCards((state) =>
+      state.map((currentCard) =>
+        currentCard._id === card._id ? newCard : currentCard
+      )
+    );
+  }
+
+  async function handleCardDelete(card) {
+    await api.deleteCard(card._id);
+    setCards((state) => state.filter((currentCard) => currentCard._id !== card._id));
+  }
+
+  async function handleCardSubmit(data) {
+    const newcard = await api.addCard(data);
+    console.log(`new cards after submition ${newcard}`)
+    setCards([newcard,...cards]);
+    console.log(cards);
+    
+  }
+
   return (
     <>
-    <CurrentUserContext.Provider value={{currentUser, setUser, handleUserUpdate, handleUpdateAvatar}}>
+    <CurrentUserContext.Provider value={{currentUser, setUser, handleUserUpdate, handleUpdateAvatar, handleCardSubmit,setCards}}>
       <div className="page">
         <Header/>
         <Main onClosePopup={handleClosePopup}
               onOpenPopup={handleOpenPopup}
-              popup={popup}/> 
+              popup={popup}
+              cards={cards}
+              onCardLike={handleCardLike}
+              onCardDelete={handleCardDelete}/> 
         <Footer/>
       </div>
       </CurrentUserContext.Provider>
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -10,42 +10,13 @@ import { CurrentUserContext } from '../../contexts/CurrentUserContext.js'
 
 export default function Main(props) {
 
-  const {onOpenPopup,onClosePopup,popup} = props
+  const {onOpenPopup,onClosePopup,popup,cards,onCardLike,onCardDelete} = props
   const newCardPopup = { title: "Nuevo lugar", children: <NewCard /> };
   const newAvatar = {title: "Editar Foto de Perfil" , children:<NewAvatar/>}
   const newProfile = {title:"Editar Perfil" ,  children:<NewProfile/>}
-  const [cards, setCards] = useState([]);
+  
   const {currentUser} = useContext(CurrentUserContext);
 
-  useEffect(() => {
-    api.getInitialCards().then((res) => {
-      console.log(res);
-      setCards(res);
-    })
-  }, []);
-
-  async function handleCardLike(card) {
-    const isLiked = card.isLiked;
-  
-    const newCard = isLiked
-      ? await api.removeLike(card._id)
-      : await api.addLike(card._id);
-  
-    setCards((state) =>
-      state.map((currentCard) =>
-        currentCard._id === card._id ? newCard : currentCard
-      )
-    );
-  }
-
-
-  async function handleCardDelete(card) {
-    await api.deleteCard(card._id);
-    setCards((state) => state.filter((currentCard) => currentCard._id !== card._id));
-  }
-
-  
-  
   function handleCardClick(card){
     onOpenPopup({title:"OpenImg" ,children:<ImgPopup card={card}/>})
 
@@ -75,8 +46,8 @@ export default function Main(props) {
               card={card} 
               isLiked={card.isLiked}
               handleCardClick={handleCardClick}
-              onCardLike={handleCardLike}
-              onCardDelete={handleCardDelete}
+              onCardLike={onCardLike}
+              onCardDelete={onCardDelete}
               />
               ))}
             </ul>
